bfb3f99e1f93edaee0772d6bc18419c98eb82fe4
libeng <928082894@qq.com>
2023-04-04 20:12:18 +0800

    The fifth step of rougelike game

--- RoguelikeGame/procgen.py
+++ RoguelikeGame/procgen.py
@@ -1,5 +1,6 @@
 import tcod
 import random
+from RoguelikeGame import entity_factories
 from typing import Iterator, List, Tuple, Type
 from RoguelikeGame.game_map import GameMap
 from RoguelikeGame import tile_types
@@ -35,6 +36,22 @@ class RectangularRoom:
         )
 
 
+def place_entities(
+        room: RectangularRoom, dungeon: GameMap, maximum_monsters: int,
+) -> None:
+    number_of_monsters = random.randint(0, maximum_monsters)
+
+    for i in range(number_of_monsters):
+        x = random.randint(room.x1 + 1, room.x2 - 1)
+        y = random.randint(room.y1 + 1, room.y2 - 1)
+
+        if not any(entity.x == x and entity.y == y for entity in dungeon.entities):
+            if random.random() < 0.8:
+                entity_factories.orc.spawn(dungeon, x, y)
+            else:
+                entity_factories.orc.spawn(dungeon, x, y)
+
+
 def tunnel_between(
         start: Tuple[int, int], end: Tuple[int, int]
 ) -> Iterator[Tuple[int, int]]:
@@ -61,11 +78,12 @@ def generate_dungeon(
         room_max_size: int,
         map_width: int,
         map_height: int,
+        max_monsters_per_room: int,
         player: Entity,
 ) -> GameMap:
     """Generate a new dungeon map."""
 
-    dungeon = GameMap(map_width, map_height)
+    dungeon = GameMap(map_width, map_height, entities=[player])
 
     rooms: List[RectangularRoom] = []
 
@@ -94,6 +112,8 @@ def generate_dungeon(
             # Dig out a tunnel between this room and the previous one.
             for x, y in tunnel_between(rooms[-1].center, new_room.center):
                 dungeon.tiles[x, y] = tile_types.floor
+
+        place_entities(new_room, dungeon, max_monsters_per_room)
 
         # Finally, append the new room to the list.
         rooms.append(new_room)
